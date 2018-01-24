2ab290845ddd089c72f32559364ac44222477eb6
Victor Lopez (UST <U34006@ust-global.com>
2018-01-24 16:10:59 -0600

    First JUnit created

--- spring-login/src/test/java/com/watts/controller/UserControllerTest.java
+++ spring-login/src/test/java/com/watts/controller/UserControllerTest.java
@@ -1,20 +1,55 @@
 package com.watts.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.watts.controller.UserController;
+import com.watts.repository.User;
+import com.watts.service.UserServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-//		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-//		UserController userController = appContext.getBean("userController", UserController.class);
-//		
-//		System.out.println(userController.userPassController("", "").getLastName());
+	
+	@InjectMocks private UserController userController;
+	@Mock private UserServiceImpl userService;
+	
+	private final static String USER = "W@tts";
+	private final static String PASS = "(takeUrT1me)";
+	private final static String FIRST_NAME = "Mijael";
+	private final static String LAST_NAME = "Watts";	
+	
+	@Test public void validateCorrectResponseWhenCredentialsNotBlank() {
+		when(userService.userAndPassFiltering(any(String.class), any(String.class))).thenReturn(createCorrectReturnedObject());
 		
-		// Close appContext
+		User user = userController.credentialsNotBlank(USER, PASS);
+		
+		assertEquals(FIRST_NAME, user.getFirstName());
+		assertEquals(LAST_NAME, user.getLastName());
+	}
+	
+	@Ignore public void validateBlankCredentialsErrorWhenCredentialsAreBlank() {}
+	
+	@Ignore public void validateBlankUserErrorWhenUserIsBlank() {}
+	
+	@Ignore public void validateBlankPasswordErrorWhenPasswordIsBlank() {}
+	
+	@Ignore public void validateWrongCredentialsErrorWhenCredentialsAreWrong() {}
+	
+	private User createCorrectReturnedObject() {
+		User user = new User();
+		
+		user.setFirstName(FIRST_NAME);
+		user.setLastName(LAST_NAME);
+		
+		return user;
 	}
 
 }
