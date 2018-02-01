645f6ea6f05b42278b5a74dcdd02fc20c8b64813
Victor Lopez (UST <U34006@ust-global.com>
2018-02-01 15:33:16 -0600

    JUnits updated according to the UserController.java changes.

--- spring-login/src/test/java/com/watts/controller/UserControllerTest.java
+++ spring-login/src/test/java/com/watts/controller/UserControllerTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,39 +11,68 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.watts.controller.UserController;
-import com.watts.repository.User;
 import com.watts.service.UserServiceImpl;
+import com.watts.user.service.LoginRequest;
+import com.watts.user.service.LoginResponse;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 	
 	@InjectMocks private UserController userController;
+	
 	@Mock private UserServiceImpl userService;
+//	private MockMvc mvc;
 	
 	private final static String USER = "W@tts";
 	private final static String PASS = "(takeUrT1me)";
 	private final static String FIRST_NAME = "Mijael";
 	private final static String LAST_NAME = "Watts";	
 	
-	@Test public void validateCorrectResponseWhenCredentialsNotBlank() {
+//	@Before
+//	public void setUp() {
+//		mvc = MockMvcBuilders.standaloneSetup(userController).setControllerAdvice(new ErrorController()).build();
+//	}
+	
+	@Test 
+	public void validateCorrectResponseWhenCredentialsNotBlank() {
+		LoginRequest login = new LoginRequest(USER, PASS);
+		
 		when(userService.userAndPassFiltering(any(String.class), any(String.class))).thenReturn(createCorrectReturnedObject());
 		
-		User user = userController.credentialsNotBlank(USER, PASS);
+		LoginResponse user =  (LoginResponse) userController.signIn(login);
 		
 		assertEquals(FIRST_NAME, user.getFirstName());
 		assertEquals(LAST_NAME, user.getLastName());
 	}
 	
-	@Ignore public void validateBlankCredentialsErrorWhenCredentialsAreBlank() {}
+	@Test(expected=IllegalArgumentException.class) 
+	public void throwIllegalArgumentExceptionWhenCredentialsAreBlank() {
+		LoginRequest login = new LoginRequest("", "");
+		userController.signIn(login);
+	}
 	
-	@Ignore public void validateBlankUserErrorWhenUserIsBlank() {}
+	@Test(expected=IllegalArgumentException.class) 
+	public void throwIllegalArgumentExceptionWhenUserIsBlank() {
+		LoginRequest login = new LoginRequest("", PASS);
+		userController.signIn(login);
+	}
+
+	@Test(expected=IllegalArgumentException.class) 
+	public void throwIllegalArgumentExceptionWhenPasswordIsBlank() {
+		LoginRequest login = new LoginRequest(USER, "");
+		userController.signIn(login);
+	}
 	
-	@Ignore public void validateBlankPasswordErrorWhenPasswordIsBlank() {}
+//	@Test(expected=IllegalArgumentException.class) 
+//	public void validateBlankCredentialsErrorWhenCredentialsAreBlank2() {
+//		LoginRequest login = new LoginRequest();
+//		login.setUser("");
+//		login.setPassword("");
+//		
+//	}
 	
-	@Ignore public void validateWrongCredentialsErrorWhenCredentialsAreWrong() {}
-	
-	private User createCorrectReturnedObject() {
-		User user = new User();
+	private LoginResponse createCorrectReturnedObject() {
+		LoginResponse user = new LoginResponse();
 		
 		user.setFirstName(FIRST_NAME);
 		user.setLastName(LAST_NAME);
