2f2b82001019e34e59002f64664c28e3da0a5b8e
MijaelWatts <elyon.systems.mijael@gmail.com>
2018-02-14 23:47:54 -0600

    App component updated.

--- angular5-login/src/app/app.component.ts
+++ angular5-login/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <header-component></header-component>
+    <body-component></body-component>
+    <footer-component></footer-component>
+  `
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
