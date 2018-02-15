1d65c962214f52b92239272645b77ff20c015b56
MijaelWatts <elyon.systems.mijael@gmail.com>
2018-02-14 23:47:38 -0600

    Created components registered in module.

--- angular5-login/src/app/app.module.ts
+++ angular5-login/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule }              from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgbModule }                  from '@ng-bootstrap/ng-bootstrap';
-// import { AlertModule }				  from 'ngx-bootstrap/alert';
+// import { TabsModule }				  from 'ngx-bootstrap/tabs';
+
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot()
-    // AlertModule.forRoot()
+    // TabsModule.forRoot()
   ],
   schemas:[ NO_ERRORS_SCHEMA ],
   providers: [],
