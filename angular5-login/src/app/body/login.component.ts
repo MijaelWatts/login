5d37319e8a46102f28b088a34e1b4e6b88732d0f
MijaelWatts <elyon.systems.mijael@gmail.com>
2018-02-16 02:46:44 -0600

    Basic set up for validating login.component.html

--- angular5-login/src/app/body/login.component.ts
+++ angular5-login/src/app/body/login.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from '../interfaces/user.interface';
 
 @Component({
 	selector: 'login-component',
 	templateUrl: './login.component.html'
 })
 
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  ngOnInit() { 
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[^ @]*@[^ @]*")
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    });
+  }
+
+}
