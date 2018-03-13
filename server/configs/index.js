fe08ce49e184aa3a2ccf09e78f53cc8ebfa1941d
Victor Lopez (UST <U34006@ust-global.com>
2018-03-13 14:54:00 -0600

    Basic server

---
+++ server/configs/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const LOCAL_CONFIG = {
+  hostname: 'localhost',
+  port: '4000'
+};
+
+module.exports = LOCAL_CONFIG;
---
+++ server/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const express = require('express');
+const parser  = require('body-parser');
+const configs  = require('./configs');
+const xprs    = express();
+
+xprs.use(parser.urlencoded({extended : true}));
+xprs.use(parser.json({type : 'application/json'}));
+
+xprs.set('hostname', configs.hostname);
+xprs.set('port', configs.port);
+
+xprs.get('/', (req, res) => {
+  res.json({
+    msg: 'Welcome to the express-firebase-login project.'
+  });
+});
+
+xprs.post('/login', (req, res) => {
+  let user = req.body.user === 'mw' ? true : false;
+  let pass = req.body.pass === 'mijaelwatts' ? true : false;
+
+  if (user && pass) {
+      res.json({msg : 'access granted.'});
+  } else {
+      res.json({msg : 'access not granted.'});
+  }
+});
+
+xprs.listen(configs.port, () => {
+  console.info("... Server listening on http://" + xprs.get('hostname') + ":" + xprs.get('port'));
+});
+
+module.exports = xprs;
+
+
+
+
+
+// Below is another way of exporting... Not yet sure which one is best.
+/*
+let create = function(config) {
+  console.log("Into create, config file: " + config);
+};
+
+let start = function() {
+  console.info("Starting the server...");
+}
+
+let object = {
+  create: create,
+  start: start
+}
+
+module.exports = object;
+*/
